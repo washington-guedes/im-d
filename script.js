d158fdb183dffc2085fb7a209a9dfd66c48aa02e
Washington Guedes <avwl017@gmail.com>
2020-12-04 00:26:25 -0300

    ajusta performance e filtros

--- script.js
+++ script.js
@@ -1,93 +1,138 @@
 
-load.onclick = read
-nrows.onchange = read
-ncols.onchange = read
-grayscale.onchange = read
+photo.onchange = loadImage
+nrows.onchange = distort
+ncols.onchange = distort
+grayscale.onchange = distort
+alpha.onchange = distort
 
-function read() {
-  const _in = new FileReader()
+let uploading = false
+let running = false
+let image = null
+
+function loadImage() {
+  if (!photo.files || !photo.files[0]) {
+    return console.warn('Missing photo')
+  }
+  if (uploading) {
+    return
+  }
+  uploading = true
+  console.time('loading image')
+  
   const img = new Image()
   img.onload = function () {
     output.width = this.width
     output.height = this.height
-    console.log(this.width, this.height)
-
-    const ctx = output.getContext('2d')
-    ctx.drawImage(img, 0, 0)
-    const image = ctx.getImageData(0, 0, this.width, this.height)
-    mutate(image.data)
-    ctx.putImageData(image, 0, 0)
+    image = img
+    console.timeEnd('loading image')
+    uploading = false
+    distort()
   }
+  
+  const _in = new FileReader()
   _in.onload = () => img.src = _in.result
   _in.readAsDataURL(photo.files[0])
 }
 
-function mutate(pixels) {
+function distort() {
+  if (!image) {
+    return console.warn('Missing image')
+  }
+  if (+nrows.value < 1 || +ncols.value < 1) {
+    return console.warn('Invalid block size')
+  }
+  if (running) {
+    return
+  }
+  running = true
+  console.time('distortion')
+  console.log(nrows.value, ncols.value, grayscale.checked, alpha.checked)
+
+  const ctx = output.getContext('2d')
+  ctx.drawImage(image, 0, 0)
+
+  const canvasImage = ctx.getImageData(0, 0, output.width, output.height)
+  const pixels = canvasImage.data
+
   const m = output.height
   const n = output.width * 4
 
-  const arr = make(nrows.value, _ => make(ncols.value, pixel))
+  const arr = make(nrows.value, _ => make(ncols.value, _ => new Pixel()))
 
-  for (let i = 0; i < m; i++) {
-    const row = (i / (m / nrows.value)) | 0
+  const loop = fn => {
+    for (let i = 0; i < m; i++) {
+      const row = (i / (m / nrows.value)) | 0
 
-    for (let j = 0; j < n; j += 4) {
-      const k = i * n + j
-      const col = (j / (n / ncols.value)) | 0
+      for (let j = 0; j < n; j += 4) {
+        const k = i * n + j
+        const col = (j / (n / ncols.value)) | 0
 
-      arr[row][col].add(...pixels.slice(k, k + 4))
+        fn(k, arr[row][col])
+      }
     }
   }
 
-  console.log(arr.map(x => x.map(y => y.grayscale())))
+  loop((k, item) => {
+    item.add(pixels[k], pixels[k+1], pixels[k+2], pixels[k+3])
+  })
 
-  for (let i = 0; i < m; i++) {
-    const row = (i / (m / nrows.value)) | 0
-
-    for (let j = 0; j < n; j += 4) {
-      const k = i * n + j
-      const col = (j / (n / ncols.value)) | 0
-
-      const x = arr[row][col];
-      const [r, g, b, a] = x.grayscale()
-      pixels[k] = r
-      pixels[k+1] = g
-      pixels[k+2] = b
-      pixels[k+3] = a
+  loop((k, item) => {
+    if (grayscale.checked) {
+      if (alpha.checked) {
+        pixels[k] = pixels[k+1] = pixels[k+2] = pixels[k+3] = item.avg_rgba()
+      } else {
+        pixels[k] = pixels[k+1] = pixels[k+2] = item.avg_rgb()
+        pixels[k+3] = item.avg_a()
+      }
+    } else {
+      pixels[k] = item.avg_r()
+      pixels[k+1] = item.avg_g()
+      pixels[k+2] = item.avg_b()
+      pixels[k+3] = item.avg_a()
     }
-  }
+  })
 
-  console.log('done')
+  ctx.putImageData(canvasImage, 0, 0)
+
+  console.timeEnd('distortion')
+  running = false
 }
 
 function make(len, mapper) {
   return [...new Array(+len)].map(mapper)
 }
 
-function pixel() {
-  let [_r, _g, _b, _a, _len] = [0, 0, 0, 0, 0]
-  const _obj = {}
-  const cache = (fn, $ = '') => {
-    const key = _len + $
-    return _obj[key] = _obj[key] || fn()
-  }
-  return {
-    add(r, g, b, a) {
-      _r += r
-      _g += g
-      _b += b
-      _a += a
-      _len++
-    },
-    average() {
-      return cache(_ => [_r, _g, _b, _a].map(x => (x / _len) | 0), 'avg')
-    },
-    grayscale() {
-      return make(4, _ => cache(_ => (sum(this.average()) / 4) | 0, 'b&w'))
-    }
-  }
-}
+class Pixel {
+  r = 0
+  g = 0
+  b = 0
+  a = 0
+  len = 0
 
-function sum(arr) {
-  return arr.reduce((acc, x) => acc + x, 0)
+  add(r, g, b, a) {
+    this.r += r
+    this.g += g
+    this.b += b
+    this.a += a
+    this.len++
+  }
+
+  avg_r() {
+    return this.r / this.len | 0
+  }
+  avg_g() {
+    return this.g / this.len | 0
+  }
+  avg_b() {
+    return this.b / this.len | 0
+  }
+  avg_a() {
+    return this.a / this.len | 0
+  }
+  avg_rgb() {
+    return (this.r + this.g + this.b) / 3 / this.len | 0
+  }
+  avg_rgba() {
+    return (this.r + this.g + this.b + this.a) / 4 / this.len | 0
+  }
 }
