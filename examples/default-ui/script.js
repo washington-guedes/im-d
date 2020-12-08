8c3c124ec1f7b1802b8449e2b24cf2f91fbacff6
Washington Guedes <avwl017@gmail.com>
2020-12-08 11:43:59 -0300

    added auto loaded image example

--- examples/default-ui/script.js
+++ examples/default-ui/script.js
@@ -1,4 +1,10 @@
 imageDistortion.createDefaultUI({
   wrapper: document.getElementById('image-distortion--wrapper'),
-  log: true
+  log: true,
+  image: (() => {
+    const img = new Image()
+    img.crossOrigin = 'Anonymous'
+    img.src = 'https://images-na.ssl-images-amazon.com/images/I/41HXUK8edZL.png'
+    return img
+  })()
 })
