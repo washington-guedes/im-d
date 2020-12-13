fedc39d3347d45bad60c04fe1a6aad2ea6624c69
Washington Guedes <avwl017@gmail.com>
2020-12-12 23:08:07 -0300

    build when starting serve

--- index.js
+++ index.js
@@ -2,17 +2,27 @@ const { watch } = require('fs')
 const { exec } = require('child_process')
 
 watch('./src', { recursive: true }, (e, filename) => {
-  console.log('-'.repeat(80))
+  hr()
   console.log(new Date().toLocaleString(), e, filename)
-  
+  build(() => hr())
+})
+
+function hr() {
+  console.log('-'.repeat(80))
+}
+
+function build (callback) {
   exec('npm run build', (err, stdout, stderr) => {
     if (err) {
       console.error(stderr || err)
       return
     }
-    console.log((stdout.match(/^(webpack|WARNING).*/gm) || []).join('\n'))
+    console.log(stdout)
     console.log(new Date().toLocaleString(), 'build finished \n')
-  })
-})
 
-console.log('Development mode started. Have fun :D \n')
+    callback()
+  })
+}
+
+build(() => console.log('Development mode started. Have fun :D \n'))
+
